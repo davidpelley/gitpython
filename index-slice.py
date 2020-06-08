f4ac8ff7bfcb1d7503d6c963fce18d2ccd23fcd9
David Pelley <davidapelley@gmail.com>
2020-06-08 14:06:42 -0500

    indexing and slicing

---
+++ index-slice.py
@@ -0,0 +1,24 @@
+## indexing
+mystring = "Hello World"
+#a = mystring[0]
+#b = mystring[8]
+#print (a,b)
+## 
+##indexing [start:stop:step]
+##
+mystring = "abcdefghijk"
+print (mystring)
+a = mystring[2:]
+print(a)
+b = mystring[:3]
+print(b)
+c = mystring[3:6]
+print(c)
+d = mystring[::2]
+print(d)
+e = mystring[2:7:2]
+print(e)
+f = mystring[::-1]
+print(f)
+
+## slicing
